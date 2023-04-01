27099c36ae330829c88c24e84e8cd8c6404af169
KOVDOM <kovacsdominik63@gmail.com>
2023-04-01 11:22:14 +0200

    pilotak faladat

---
+++ Pilotak/Adatok.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilotak
+{
+    internal class Adatok
+    {
+        public string nev { get; private set; }
+        public DateTime szul_datum { get; private set; }
+        public string nemzet { get; private set; }
+        public int rajtszam { get; private set; }
+
+        public Adatok(string nev, DateTime szul_datum, string nemzet, int rajtszam)
+        {
+            this.nev = nev;
+            this.szul_datum = szul_datum;
+            this.nemzet = nemzet;
+            this.rajtszam = rajtszam;
+        }
+
+        public Adatok (string sor)
+        {
+            string[] darabok= sor.Split(';');
+            nev= darabok[0];
+            szul_datum = Convert.ToDateTime(darabok[1]);
+            nemzet = darabok[2];
+            if (!string.IsNullOrEmpty(darabok[3]))
+            {
+                this.rajtszam = int.Parse(darabok[3]);
+            }
+        }
+    }
+}
---
+++ Pilotak/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Pilotak
+{
+    internal class Program
+    {
+        static List<Adatok> list=new List<Adatok>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("pilotak.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                Adatok adatok=new Adatok(sr.ReadLine());
+                list.Add(adatok);
+            }
+            sr.Close();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+            Feladat6();
+            Feladat7();
+            
+            Console.ReadKey();
+        }
+
+        public static void Feladat3()
+        {
+            Console.WriteLine($"3. feladat {list.Count()}");
+        }
+
+        public static void Feladat4()
+        {
+            Console.WriteLine($"4. Feladat {list.Last().nev}");
+        }
+
+        public static void Feladat5()
+        {
+            Console.WriteLine($"5. feladat");
+            list.Where(cx => cx.szul_datum < DateTime.Parse("1901.01.01")).ToList().ForEach(z => Console.WriteLine($"\t{z.nev} {z.szul_datum.ToShortDateString()})"));
+        }
+
+        public static void Feladat6()
+        {
+            Console.WriteLine($"6. Feladat {list.FindAll(v=>v.rajtszam > 0).OrderBy(e=>e.rajtszam).First().nemzet}");
+        }
+
+        public static void Feladat7()
+        {
+            Console.WriteLine($"7. feladat");
+            list.GroupBy(r => r.rajtszam).Where(g => g.Count() > 1 && g.Key != 0).ToList().ForEach(t => Console.Write(t.Key + " "));
+        }
+    }
+}
