98416dfbb376b63bb25803d1dcdb011bbf9e2d86
KOVDOM <kovacsdominik63@gmail.com>
2023-04-07 15:18:18 +0200

    jackie

---
+++ Adatok.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackieStewart
+{
+    internal class Adatok
+    {
+        public int year { get; private set; }
+        public int races { get; private set; }
+        public int wins { get; private set; }
+        public int podiums { get; private set; }
+        public int poles { get; private set; }
+        public int fastests { get; private set; }
+
+        public Adatok(string sor)
+        {
+            string[] arr = sor.Split('\t');
+            year = int.Parse(arr[0]);
+            races = int.Parse(arr[1]);
+            wins = int.Parse(arr[2]);
+            podiums = int.Parse(arr[3]);
+            poles = int.Parse(arr[4]);
+            fastests = int.Parse(arr[5]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackieStewart
+{
+    internal class Program
+    {
+        static List<Adatok> list = new List<Adatok>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("jackie.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                Adatok adatok = new Adatok(sr.ReadLine());
+                list.Add(adatok);
+            }
+            sr.Close();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+            Feladat6();
+
+            Console.ReadKey();
+        }
+
+        public static void Feladat3()
+        {
+            Console.WriteLine($"3. feladat: {list.Count()}");
+        }
+
+        public static void Feladat4()
+        {
+            Console.WriteLine($"4. feladat: {list.OrderBy(cx=>cx.races).Last().year}");
+        }
+
+        public static void Feladat5()
+        {
+            int elott = 0;
+            int utan = 0;
+            foreach (var item in list) 
+            {
+                if (item.year<1970)
+                {
+                    elott+=item.wins;
+                }
+                else
+                {
+                    utan+=item.wins;
+                }
+            }
+            Console.WriteLine($"5. feladat\n\t70es évek: {utan} megnyert verseny\n\t60-as évek: {elott} megnyert verseny");
+
+            Console.WriteLine($"\t70-es évek: {list.Where(x=>x.year>= 1970 && x.year<1980).Sum(y=>y.wins)} megnyert verseny");
+            Console.WriteLine($"\t60-as évek: {list.Where(x=>x.year>=1960 && x.year<1970).Sum(y=>y.wins)} megnyert verseny");
+        }
+
+        public static void Feladat6()
+        {
+            StreamWriter sw = new StreamWriter("jackie.html");
+            sw.WriteLine("<!doctype html>");
+            sw.WriteLine("<html>");
+            sw.WriteLine("<head></head>");
+            sw.WriteLine("<style>td {border:1px solid black}</style>");
+            sw.WriteLine("<body>");
+            sw.WriteLine("<h1>Jackie Stewrt</h1>");
+            sw.WriteLine("<table>");
+            foreach (var item in list)
+            {
+                sw.WriteLine($"<tr><td>{item.year}</td><td>{item.races}</td><td>{item.wins}</td></tr>");
+            }
+            sw.WriteLine("</table>");
+            sw.WriteLine("</body>");
+            sw.WriteLine("</html>");
+            sw.Close();
+            Console.WriteLine("6. feladat jackie.html");
+        }
+    }
+}
